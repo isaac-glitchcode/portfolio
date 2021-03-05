7030224b46194cfe97af823c0ebdf9c01cc64bcf
isaac-glitchcode <isaac.su@gmx.com>
2021-03-04 19:39:11 -0600

    Fixed: Call to file that no exist

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Load from './components/loading/loading'
 import Logo from './components/loading/img/logo.png'
 import Main from './components/main/main';
-import Img from './components/main/img/im.jpg';
+// import Img from './components/main/img/im.jpg';
 import About from './components/main/img/About.jpg';
 import Portfolio from './components/main/img/Portfolio.jpg';
 import Extras from './components/main/img/Extras.jpg';
