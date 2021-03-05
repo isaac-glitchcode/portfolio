cc9ae9a0d22e3ba964ac9fec52099a81020c5f03
isaac-glitchcode <isaac.su@gmx.com>
2021-03-04 20:12:18 -0600

    Fixed: Add landscape to no tunr on phone

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Loading(props){
     
     return(
-        <section className="content-load">
+        <section className="content-load ">
+            <div className="landscape">
+                <h1>Turn on your  phone</h1>
+            </div>
             <img className="logo" src={props.logo} alt="Bg"/>
             <div className="progress"></div>
             <input 
